d2a730515560882d207793856b516169a7accca4
YashInCode <mr.yashmr.gupta@gmail.com>
2025-01-13 00:16:29 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,139 @@
+const logotext = "YASH";
+const meta = {
+    title: "Yash Gupta",
+    description: "I’m Yash Gupta_ Full stack developer,currently Staying in Bengaluru",
+};
+
+const introdata = {
+    title1: "I’m Yash ",
+    title2: " Gupta",
+    animated: {
+        first: "MERN Stack Developer",
+        second: "Passionate About Coding",
+        third: "I develop Web Application",
+    },
+    description: "I'm a Web-Desigener & MERN Stack Web Devloper focused on crafting , clean & user-friendly Experience , I am passionate about building exellent Software that improve the life ofthose around me. ",
+    your_img_url: "https://scontent.fblr1-7.fna.fbcdn.net/v/t51.75761-15/462741186_18354059344186167_7473531393114012916_n.jpg?stp=dst-jpegr_tt6&_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=2DWX7VrTbz4Q7kNvgFbLXqI&_nc_zt=23&se=-1&_nc_ht=scontent.fblr1-7.fna&_nc_gid=A2s1KNWIbX8qqyTTQlWQ3bj&oh=00_AYDyxbszLr49O_2g7w4Hb59qRj8NYm6dozqjjRqe1pfnJw&oe=678954E1",
+};
+
+const dataabout = {
+    title:  "About my self",
+    aboutme: "Having 6 Months experience in web development specializing in html, css, bootstrap, javascript, jquery, reactjs, nodejs, API, Mysql, xml, json, ajax, jquery ajax, web services Experienced in development, and Web based applications. Well knowledge of Software Development Life Cycle, and well knowledge of module testing and module integration. Well knowledge of errors detecting and finding the bugs. Experience in web maintenance and good understanding in enhancement of requirements. Communicates with customers and offshore team members effectively. Excellent Team Player possessing Good Communication skills.",
+};
+const worktimeline = [
+    {
+        jobtitle: "MERN Stack Developer (Intern)",
+        where: "InfoCampus Logics Pvt. Ltd.",
+        date: "2024",
+    },
+    {
+        jobtitle: "Bachelor of Science",
+        where: "Chhatrapati Shahu Ji Maharaj University",
+        date: "2021",
+    },
+    {
+        jobtitle: "SSC",
+        where: "State Board",
+        date: "2018",
+    },
+    
+];
+
+const skills = [{
+        name: "HTML",
+        value: 70,
+    },
+    {
+        name: "Css",
+        value: 65,
+    },
+    {
+        name: "Bootstarp",
+        value: 73,
+    },
+    {
+        name: "Javascript",
+        value: 55,
+    },
+    {
+        name: "React",
+        value: 68,
+    },
+];
+
+const services = [{
+        title: "Frontend Development ",
+        description: "I design fully responsive websites using HTML5, CSS3, Bootstarp and JavaScript, while building modern, scalable web applications with React.js for optimal performance and usability. I focus on UI/UX design, cross-browser compatibility, and implement performance optimization techniques and image optimization for a seamless, fast user experience.",
+    },
+    {
+        title: "MERN Stack Development Services",
+        description: "I develop full-stack web applications using the MERN stack, ensuring seamless integration between React.js, Node.js, Express.js, and MongoDB. I also create RESTful APIs, manage scalable databases, and build real-time applications for dynamic, interactive user experiences.",
+    },
+    {
+        title: "Technologies & Tools",
+        description: "I am using React.js, Redux, HTML5, CSS3, Bootstrap, JavaScript, and build robust Backend services with Node.js, Express.js and MongoDB,. I utilize Git for version control, and deploy applications on platforms like Netlify for seamless performance and scalability..",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://www.facebook.com/d5041ba0-35c0-423d-a940-fdbd08df6360",
+        description: "A comprehensive README file providing an overview, setup instructions, and usage details for the project hosted on GitHub.",
+        link: "https://github.com/YashInCode/YashInCode",
+    },
+    {
+        img: "https://www.facebook.com/d5041ba0-35c0-423d-a940-fdbd08df6360",
+        description: "User-friendly job application portal designed to streamline the application process, allowing candidates to easily apply for positions.",
+        link: "https://github.com/YashInCode/Job-Portal-HIRE-SPIRE",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "A weather app that provides real-time forecasts and weather updates by fetching data from a reliable API.",
+        link: "https://github.com/YashInCode/Weather-App",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "User authentication API built with MongoDB for secure registration, login, and session management.",
+        link: "https://github.com/YashInCode/User_Authentication_APIs_MongoDb",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "User login system built with React and Redux for efficient state management and seamless authentication flow",
+        link: "https://github.com/YashInCode/redux",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: " CRM system designed to manage and analyze customer interactions, streamline processes, and improve business relationships.",
+        link: "https://github.com/YashInCode/Company_Billing_Appp",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mr.yashmr.gupta@gmail.com",
+    YOUR_FONE: "8009505008",
+    description: "Feel Free To Get In touch With Me, I Am Always Open to Discussing new Projects, Creative Ideas or Opportunities to be part of your Vision. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_u4uky6n",
+    YOUR_TEMPLATE_ID: "template_c1g8kej",
+    YOUR_USER_ID: "dbcL8W-EMQq9krEvz",
+};
+
+const socialprofils = {
+    github: "https://github.com/YashInCode",
+    facebook: "https://www.facebook.com/profile.php?id=100067177076531",
+    linkedin: "https://www.linkedin.com/in/yash-gupta-0a3587332/",
+    twitter: "https://x.com/badfitt"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
